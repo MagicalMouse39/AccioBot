61fe2114cc60f82e6baecf3a21d22c2da64709dd
MagicalMouse39 <31729116+MagicalMouse39@users.noreply.github.com>
2022-03-22 19:58:27 +0100

    UPDATE: ffmpepega.sh

---
+++ Tests/TemplateVR/TestServer/ffmpepega.sh
@@ -0,0 +1,17 @@
+cd ~/FFmpeg && \
+git -C SVT-AV1 pull 2> /dev/null || git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git && \
+mkdir -p SVT-AV1/build && \
+cd SVT-AV1/build && \
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF .. && \
+PATH="$HOME/bin:$PATH" make && \
+make install && \
+sudo apt-get install python3-pip && \
+pip3 install --user meson && \
+cd ~/FFmpeg && \
+wget https://github.com/Netflix/vmaf/archive/v2.1.1.tar.gz && \
+tar xvf v2.1.1.tar.gz && \
+mkdir -p vmaf-2.1.1/libvmaf/build &&\
+cd vmaf-2.1.1/libvmaf/build && \
+meson setup -Denable_tests=false -Denable_docs=false --buildtype=release --default-library=static .. --prefix "$HOME/ffmpeg_build" --bindir="$HOME/ffmpeg_build/bin" --libdir="$HOME/ffmpeg_build/lib" && \
+ninja && \
+ninja install
